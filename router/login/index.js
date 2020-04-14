a2f20baca3ae72995cc7066b29f879f544f6ebfa
taehyundev <K.anon.11107@gmail.com>
2020-04-14 20:13:02 +0900

    update 2020-04-14 login jwt로 해보기

--- router/login/index.js
+++ router/login/index.js
@@ -1,11 +1,8 @@
 var express = require('express')
+var models = require('../../config/jwt')
 var router = express.Router()
 
 router.get('/', function(req,res){
-    res.render('login.ejs')
-})
-
-router.post('/', function(req,res){
     res.render('login.ejs')
 })
 
